b3655b6f672eeb91705e1cbe325fa659e89b1f00
femosik <femosik@yandex.ru>
2019-05-12 21:37:00 +0300

    Update 0.9.1: Completely changed the page wit detalied information about the category; Top added navigation buttons

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
+from books.models import Book
 from categories.models import Category
 
 
@@ -23,19 +25,11 @@ def show_categories(request):
 
 def show_category_detail(request, category_id):
     category = Category.objects.get(id=category_id)
-    result = f'<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">' \
-        f'<span style="color: white;' \
-        f'font-family: sans-serif;' \
-        f'font-size: 42px;">{category.name} </span><i class="fas fa-backward"></i><hr><br>'
+    books = category.books.all
 
-    books = category.books.all()
-    for book in books:
-        result += f'<body style="background-color: #262626;"><a href="/books/{book.id}" style="' \
-            f'color: #262626;' \
-            f'background-color: white;' \
-            f'font-size: 60px;' \
-            f'margin-left: 30px;' \
-            f'border-radius: 15px;' \
-            f'padding: 10px">{book.name}</a>'
-
+    result = render_to_string('show_category_detail.html', {
+        'title': category.name,
+        'category': category,
+        'books': books,
+    })
     return HttpResponse(result)
